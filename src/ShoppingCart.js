c3d43ff92209f9e70e35b79456aa8191abfd754b
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-18 18:31:11 -0500

    Agregamos y configuramos el delete de una función

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+//Este initialCart va a ser un arreglo 
+const initialCart = [
+    { id:1, title: 'Product', description: 'Desc'},
+    { id:2, title: 'Product #2', description: 'Desc #2'}
+];
 
 function ShoppingCart() {
+    const [cart, setCart] = useState(initialCart);
+
+    const deleteProduct = (productId) => {
+
+        const changedCart = cart.filter(product => product.id !== productId);
+
+        setCart(changedCart);
+    }
+
   return (
     <div>
-        <h1>
-            Soy ShoppingCart
-        </h1>
+        {cart.map(product => (
+            <div key={product.id}>
+                <h1>{product.id} {product.title}</h1>
+                <p>{product.description}</p>
+                <button onClick={() => deleteProduct(product.id)}>
+                    Delete
+                </button>
+            </div>
+        ))}
+
+        
     </div>
   )
 }
