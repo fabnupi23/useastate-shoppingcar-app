b31b05a7189715561bfd65b57577a9f47ba2fa91
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-18 17:40:01 -0500

    Estructura proyecto shoppingcart - hooks

---
+++ src/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ShoppingCart() {
+  return (
+    <div>
+        <h1>
+            Soy ShoppingCart
+        </h1>
+    </div>
+  )
+}
+
+export { ShoppingCart };
